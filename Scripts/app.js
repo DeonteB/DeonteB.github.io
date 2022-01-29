146fa5f656ed98fdea6d5f2f9ddc0255772421d1
Deonte <deonte.bayliss@dcmail.ca>
2022-01-29 00:51:04 -0500

    Added Ice 3

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,100 +2,147 @@
 // Anonymous Self
 (function()
 {
-
-    function DisplayHomePage()
+    function DisplayHome()
     {
         console.log("Home Page");
+
         let AboutUsButton = document.getElementById("AboutUsButton");
         AboutUsButton.addEventListener("click", function()
         {
-
             location.href = "about.html";
         });
 
-        //Step.1 - get a referance to an entry point(s) (insertion / deletion)
+        // Step 1 - get a reference to an entry point(s) (insertion / deletion point)
         let MainContent = document.getElementsByTagName("main")[0];
         let DocumentBody = document.body;
-
-        //Step.2 - Create a HTML Element in memory 
+        
+        // Step 2 - Create a HTML Element in memory
         let MainParagraph = document.createElement("p");
         let Article = document.createElement("article");
-        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is Article Paragraph</p>`;
-        //Step.3 - Configure new Element
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`;
+
+        // Step 3 - Configure new Element
         MainParagraph.setAttribute("id", "MainParagraph");
         MainParagraph.setAttribute("class", "mt-3");
         let FirstString = "This is";
         let SecondString = `${FirstString} the Main Paragraph`;
         MainParagraph.textContent = SecondString;
-        Article.setAttribute("class", "contanier");
+        Article.setAttribute("class", "container");
 
 
-        //Step.4 - perform insertion / deletion
+        // Step 4 - perform insertion / deletion
 
-        //Example of insert after
+        // example of insert after (append)
         MainContent.appendChild(MainParagraph);
         Article.innerHTML = ArticleParagraph;
         DocumentBody.appendChild(Article);
-        //text content shows code while innerhtml while show html on page
-        //DocumentBody.innerHTML = `
-        //    <div class="container">
-        //    <h1 class="display-1">Hello, World!</h1>
-        //    <p class="mt-5">and...What do you think of this</p>
-        //    </div>
-       // 
-       // `;
-
-        //example of insert before
-        //MainContent.before(MainParagraph);
-        //Examle of deleting insertion so example below is of deleting the button
-        //document.getElementById("AboutUsButton").remove();
-
 
     }
 
-    function DisplayProducts()
-    {
-        console.log("Products Page");
-    }
-    function DisplayServicesPage()
-    {
-        console.log("Services Page");
-    }
     function DisplayAboutPage()
     {
-        console.log("About Page");
+        console.log("About Us Page");
     }
+
+    function DisplayProjectsPage()
+    {
+        console.log("Our Projects Page");
+    }
+
+    function DisplayServicesPage()
+    {
+        console.log("Our Services Page");
+    }
+
     function DisplayContactPage()
     {
-        console.log("Contacts Page");
+        console.log("Contact Us Page");
+
+        let sendButton = document.getElementById("sendButton");
+        let subscribeCheckbox = document.getElementById("subscribeCheckbox");
+
+        sendButton.addEventListener("click", function()
+        {
+            if(subscribeCheckbox.checked)
+            { 
+                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
+                if(contact.serialize())
+                {
+                    let key = contact.FullName.substring(0, 1) + Date.now();
+
+                    localStorage.setItem(key, contact.serialize());
+                }
+            }
+        });
     }
-    //named funcation option
-    
+
+    function DisplayContactListPage()
+    {
+        console.log("Contact-List Page");
+        if(localStorage.length > 0)
+        {
+            let contactList = document.getElementById("contactList");
+
+            let data = ""; // data container, add deserialized data from the localStorage
+
+            let keys = Object.keys(localStorage); // returns a string array of keys
+
+            let index = 1; // counts how many keys
+
+            // for every key in the keys array (collection), loop
+            for (const key of keys) 
+            {
+                let contactData = localStorage.getItem(key); // get localStorage data value related to the key
+
+                let contact = new Contact(); // create a new empty contact object
+                contact.deserialize(contactData);
+
+                // inject a repeatable row into the contactList
+                data += `<tr>
+                <th scope="row" class="text-center">${index}</th>
+                <td>${contact.FullName}</td>
+                <td>${contact.ContactNumber}</td>
+                <td>${contact.EmailAddress}</td>
+                <td></td>
+                <td></td>
+                </tr>
+                `;
+
+                index++;
+            }
+
+            contactList.innerHTML = data;
+        }
+    }
+
+
+    // named function
     function Start()
     {
-        console.log("App Started!");
+        console.log("App Started!!");
 
-        switch(document.title)
-        {
-            case "Home":
-                DisplayHomePage();
-                break;
-            case "Our Products":
-                DisplayProducts();
-                break;
-            case "Our Services":
-                DisplayServicesPage();
-                break;
-            case "About Us":
-                DisplayAboutPage();
-                break;
-            case "Contact Us":
-                DisplayContactPage();
-                break;
+        switch (document.title) {
+          case "Home":
+            DisplayHome();
+            break;
+          case "About Us":
+            DisplayAboutPage();
+            break;
+          case "Our Projects":
+            DisplayProjectsPage();
+            break;
+          case "Our Services":
+            DisplayServicesPage();
+            break;
+          case "Contact-List":
+            DisplayContactListPage();
+            break;
+          case "Contact Us":
+            DisplayContactPage();
+            break;
         }
-
-    } 
-
+    }
+    
 
     window.addEventListener("load", Start);
 
